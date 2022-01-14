4fc89c8569cf9a58a730ec28138ec297e39c2a99
Nathan Bierema <nbierema@gmail.com>
2022-01-14 15:49:23 -0500

    Code

---
+++ GlobalUsingStaticExtensionMethod/Namespace1/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace GlobalUsingStaticExtensionMethod.Namespace1;
+
+public static class Class1
+{
+    public static bool TestExtensionMethod(this int number)
+    {
+        return true;
+    }
+}
---
+++ GlobalUsingStaticExtensionMethod/Namespace2/Class2.cs
@@ -0,0 +1,9 @@
+﻿namespace GlobalUsingStaticExtensionMethod.Namespace2;
+
+public class Class2
+{
+    public void Test(int num)
+    {
+        var test = num.TestExtensionMethod();
+    }
+}
